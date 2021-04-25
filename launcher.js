47f28977d3e1a8f2dfce50223f7614cd38b87fa0
jenniferdavid <jenniferdavid87@gmail.com>
2021-04-25 01:33:05 +0100

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,36 @@
+class launcher{
+	constructor(body,anchor)
+	{
+		
+		var options={ bodyA:body,			 
+			pointB:anchor, 
+			stiffness:0.004, 
+			length:1
+			
+		}
+		
+		this.bodyA=body
+		this.pointB=anchor
+		this.launcher=Constraint.create(options)
+		World.add(world,this.launcher)
+	}
+
+	attach(bodyA){
+		this.launcher.bodyA=bodyA;
+	}
+
+	fly()
+	{
+		this.launcher.bodyA=null;
+	}
+
+	display()
+	{
+		if(this.launcher.bodyA)
+		{
+			var pointA=this.bodyA.position;
+			var pointB=this.pointB
+			line(pointA.x,pointA.y,pointB.x,pointB.y);
+		}
+	}
+}
